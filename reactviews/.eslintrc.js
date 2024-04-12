d7d9b206cb28ab6b382e354462c2f3080faabc8b
Yves AGOSTINI <yves.agostini@univ-lorraine.fr>
2024-04-12 17:39:00 +0200

    add missing eslint config files

---
+++ reactviews/.eslintrc.js
@@ -0,0 +1,58 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'node': true,
+        'es6': true
+    },
+    'parser': '@babel/eslint-parser',
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'sourceType': 'module',
+        'ecmaVersion': 6,
+        'requireConfigFile': false,
+        'babelOptions': {
+            'presets': ['@babel/preset-react']
+        },
+        'ecmaFeatures': {
+            'experimentalObjectRestSpread': true,
+            'jsx': true,
+            'modules': true
+        }
+    },
+    'plugins': [
+        'react'
+    ],
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'react/jsx-no-bind': [
+            'off'
+        ],
+        'react/jsx-indent': [
+            'off'
+        ],
+        'react/jsx-indent-props': [
+            'off'
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ],
+        'no-unused-vars': [
+            'off',
+            {
+                'ignoreRestSiblings': true
+            }
+        ]
+    }
+};
